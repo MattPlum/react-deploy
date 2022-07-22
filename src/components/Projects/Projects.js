202eb68bc9529358ca9a0b7728cb44905b931622
mattplum <mattdouglee@gmail.com>
2022-07-22 16:23:36 -0400

    updated projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,7 +4,10 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import lead_game from "../../Assets/Projects/lead_game_gif.gif";
 import old_website from "../../Assets/Projects/old_website.gif";
-
+import caesar from "../../Assets/Projects/caesar.png";
+import aes_rsa from "../../Assets/Projects/aes_rsa.png";
+import sqlinjection from "../../Assets/Projects/sqlinjection.png";
+import gdocs from "../../Assets/Projects/gdocs.png";
 
 function Projects() {
   return (
@@ -20,15 +23,6 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={5} className="project-card">
             <ProjectCard
-              imgPath={lead_game}
-              isBlog={false}
-              title="Small Java Game"
-              description="A short 2D platformer video game written in Java about the style of leadership I take when I am in a team-oriented work enviorment."
-              ghLink="https://github.com/MattPlum/gamestest"
-            />
-          </Col>
-          <Col md={5} className="project-card">
-            <ProjectCard
               imgPath={old_website}
               isBlog={false}
               title="Old Website"
@@ -38,7 +32,7 @@ function Projects() {
           </Col>
           <Col md={5} className="project-card">
             <ProjectCard
-              imgPath={lead_game}
+              imgPath={caesar}
               isBlog={false}
               title="Caesar Encryption"
               description="A CBC substitution encryption algorithm that accepts a stream of ASCII characters and encrypts this input.  "
@@ -47,7 +41,7 @@ function Projects() {
           </Col>
           <Col md={5} className="project-card">
             <ProjectCard
-              imgPath={lead_game}
+              imgPath={aes_rsa}
               isBlog={false}
               title="AES and RSA Python Encryption"
               description="Two Encryption programs in Python using AES and RSA encryption"
@@ -56,7 +50,7 @@ function Projects() {
           </Col>
           <Col md={5} className="project-card">
             <ProjectCard
-              imgPath={lead_game}
+              imgPath={sqlinjection}
               isBlog={false}
               title="SQL Injection Attack"
               description="Simple website using mySQL database to authenticate login, but is vulnerable to SQL injection attacks"
@@ -65,11 +59,20 @@ function Projects() {
           </Col>
           <Col md={5} className="project-card">
             <ProjectCard
-              imgPath={lead_game}
+              imgPath={gdocs}
               isBlog={false}
               title="Google Docs Clone"
               description="Project to immitate Google Docs allowing for multiple users to write in the same document and have it persist across sessions. Dployed the application to Google Cloud using Docker containers"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+            />
+          </Col>
+          <Col md={5} className="project-card">
+            <ProjectCard
+              imgPath={lead_game}
+              isBlog={false}
+              title="Small Java Game"
+              description="A short 2D platformer video game written in Java about the style of leadership I take when I am in a team-oriented work enviorment."
+              ghLink="https://github.com/MattPlum/gamestest"
             />
           </Col>
         </Row>
